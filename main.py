7f1fd9741e6a9610ed65f0d5f4b9f494af1a7b5b
Djoeke <djoekeduijker@live.nl>
2024-01-06 21:29:59 +0100

    Dataframe transposed and made a new df with corresponding date and shift

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 # This program will convert my schedule into a calendar file, so I don't have to manually enter my shifts and standby's
-
-# import pandas
 import pandas as pd
+
 # Read CSV file
-df = pd.read_csv('./schedules/01-2024.csv' )
-df.columns = ["Key", "Weeknummer", "Maandag", "Dinsdag","Woensdag", "Donderdag", "Vrijdag"]
+df = pd.read_csv('./schedules/01-2024.csv', engine='python', skipfooter=9, index_col=0)
 
-dates_row = df[df["Key"] == "Week"]
-shift_row = df[df["Key"] == "Shift"]
+transposed_df = df.transpose()
+# transposed_df.drop(df.columns[0], axis=1, inplace=True)
+transposed_df.reset_index(drop=True, inplace=True)
 
-# print(dates_row)
-print(shift_row)
+# Drop the first column
+transposed_df.drop(transposed_df.columns[0], axis=1, inplace=True)
+# Drop the first row
+transposed_df.drop(transposed_df.index[0], inplace=True)
 
+## Use melt to reshape the DataFrame
+melted_df = pd.melt(transposed_df, id_vars=None, var_name='Category', value_name='Value')
+
+# Extract relevant information
+dates = melted_df.loc[melted_df['Category'] == 'Week', 'Value'].reset_index(drop=True)
+shift = melted_df.loc[melted_df['Category'] == 'Shift', 'Value'].reset_index(drop=True)
+standby = melted_df.loc[melted_df['Category'] == 'Standby', 'Value'].reset_index(drop=True)
+
+# Create a new DataFrame with the reshaped data
+new_df = pd.DataFrame({
+    'Date': dates,
+    'Shift': shift,
+    'Standby': standby
+})
+print(new_df)
 
